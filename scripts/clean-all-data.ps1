f7ad3fe45f2e5f93cf4ec874dca23cfdd6637d0e
Sean Kane <Sean Kane>
2018-01-27 14:33:46 -0800

    Fix Powershell cleanup scripts

--- scripts/clean-all-data.ps1
+++ scripts/clean-all-data.ps1
@@ -1,21 +1,22 @@
 #!/usr/bin/env powershell
 
-Set-Variable BASE "'$HOMEPATH'/docker-class-201"
+$BASE = "$HOME\docker-class-201"
 
-Write-Host "This will reset all the class data under '$BASE' !!!"
+Write-Host "This will reset all the class data under $BASE !!!"
 Write-Host ""
 Write-Host "Are you sure this is what you want to do?"
 
 $confirmation = Read-Host "You must type 'yes' to confirm: "
 if ($confirmation -eq 'yes') {
-  rm '$BASE'/layout/postgres/data/data -r -fo
-  rm '$BASE'/layout/registry/data/docker -r -fo
-  rm '$BASE'/layout/jenkins/data/* -r -fo
-  rm '$BASE'/layout/jenkins/data/.groovy -r -fo
-  rm '$BASE'/layout/jenkins/data/.java -r -fo
-  rm '$BASE'/layout/gogs/data/git -r -fo
-  rm '$BASE'/layout/gogs/data/gogs -r -fo
-  rm '$BASE'/layout/gogs/data/ssh -r -fo
+  rm "$BASE/layout/postgres/data/data" -r -fo
+  rm "$BASE/layout/registry/data/docker" -r -fo
+  rm "$BASE/layout/jenkins/data/*" -r -fo
+  rm "$BASE/layout/jenkins/data/.groovy" -r -fo
+  rm "$BASE/layout/jenkins/data/.java" -r -fo
+  rm "$BASE/layout/gogs/data/git" -r -fo
+  rm "$BASE/layout/gogs/data/gogs" -r -fo
+  rm "$BASE/layout/gogs/data/ssh" -r -fo
+  cp  "$BASE/layout/postgres/data/.git_keep" "$BASE/layout/jenkins/data/.git_keep"
   Write-Host "completed"
 } else {
   Write-Host "aborted"
--- scripts/clean-jenkins-data.ps1
+++ scripts/clean-jenkins-data.ps1
@@ -1,18 +1,17 @@
 #!/usr/bin/env powershell
 
-Set-Variable BASE "'$HOMEPATH'/docker-class-201"
+$BASE = "$HOME\docker-class-201"
 
-Write-Host "This will reset all the Jenkins data under '$BASE' !!!"
+Write-Host "This will reset all the Jenkins data under $BASE !!!"
 Write-Host ""
 Write-Host "Are you sure this is what you want to do?"
 
 $confirmation = Read-Host "You must type 'yes' to confirm: "
 if ($confirmation -eq 'yes') {
-  rm '$BASE'/layout/jenkins/data/* -r -fo
-  rm '$BASE'/layout/jenkins/data/.groovy -r -fo
-  rm '$BASE'/layout/jenkins/data/.java -r -fo
-  Write-Host "completed"
+  rm "$BASE/layout/jenkins/data/*" -r -fo
+  rm "$BASE/layout/jenkins/data/.groovy" -r -fo
+  rm "$BASE/layout/jenkins/data/.java" -r -fo
+  $null > "$BASE/layout/jenkins/data/.git_keep"  cp  "$BASE/layout/postgres/data/.git_keep" "$BASE/layout/jenkins/data/.git_keep"  Write-Host "completed"
 } else {
   Write-Host "aborted"
 }
-
