4dda4323ec89fab76561289cb619594b9358ea11
Sean P. Kane <spkane00@gmail.com>
2021-01-05 12:53:21 -0800

    Fix worker script

--- terraform/bin/setup_workers.sh
+++ terraform/bin/setup_workers.sh
@@ -5,8 +5,7 @@ set -u
 
 source ./bin/ip_vars.sh 1> /dev/null
 
-export workers=$(terraform-0-11-14 output  | grep worker | cut -d " " -f 3)
-for i in $(echo $workers | sed "s/,/ /g")
+for i in "${worker_ips[@]}"
 do
   ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  -o "BatchMode=yes" -i ~/.ssh/oreilly_aws ubuntu@${i} "sudo docker swarm join --token ${SWARM_WORKER_TOKEN} ${primary_manager_ip}:2377"
 done
